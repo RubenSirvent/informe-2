b15328a9843a0294d15faee718623aca3f613b76
RubenSirvent <r.sirventu@gmail.com>
2026-08-28 20:39:16 -0400

    feat: agrega segundo test automatizado de división

--- src/test/java/informe2/DivideTest.java
+++ src/test/java/informe2/DivideTest.java
@@ -1,5 +1,15 @@
 package informe2;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class DivideTest {
+
+    @Test
+    public void testDividir() {
+        Divide divide = new Divide();
+        int resultado = divide.dividir(10, 2);
+        assertEquals(5, resultado);
+    }
     
 }
